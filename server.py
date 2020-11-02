d4b3e2ac2c151193009c71303b5d9f2ad5eeb2cc
Prashant Singh <prashantxc8@gmail.com>
2020-11-02 23:15:10 +0530

    Flask server at localhost:8000

--- server.py
+++ server.py
@@ -1,13 +1,31 @@
 """
-Flask server for time.com "The Brief" section API
+Flask server for libgenesis API
 @author prashantxc
 @licence MIT License
 """
 
 from flask import Flask, jsonify
 
+import get_book as getBook
+
 
 app = Flask(__name__)
+
+@app.route("/getBook")
+def get_url_result():
+    """API end point for Book Request"""
+    news_response = getBook.get_by_title()
+
+    return jsonify({
+        "news": news_response
+    })
+
+# Test API
+@app.route("/ok")
+def ohkay():
+    return jsonify({
+        "ok": True
+    })
 
 if __name__ == "__main__":
     app.debug = True
